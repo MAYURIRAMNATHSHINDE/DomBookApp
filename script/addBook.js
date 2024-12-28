d0e51584ec38b9e1a6d080af10e113d854833daa
MAYURIRAMNATHSHINDE <mayurishinde24304@gmail.com>
2024-12-28 17:00:27 +0530

    4th  comment

--- script/addBook.js
+++ script/addBook.js
@@ -3,13 +3,15 @@ let author=document.getElementById("author").value;
 let category=document.getElementById("selectCategory").value;
 let bookObj={title,author,category}
 
-let selectCategory=document.getElementById("selectCategory");
-selectCategory.addEventListener("change",function(){
-    fetch('https://assorted-buttoned-pike.glitch.me/books',{
-    method:"POST",
-    headers:{
-        "content-type":"application/json"
-    },
-    body:JSON.stringify(bookObj)
-})
-})
+let postBtn=document.createElement("button");
+    postBtn.textContent = "Post Product";
+    document.body.appendChild(postBtn);
+    postBtn.addEventListener("click",async function(){
+        let res=await fetch("https://assorted-buttoned-pike.glitch.me/books",{
+            method:"POST",
+            headers:{
+                "content-type":"application/json"
+            },
+            body:JSON.stringify({});
+        })
+    })
